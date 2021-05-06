efa2dcda989dedcea120442cc88583772c49ce99
KaitM1 <83773037+KaitM1@users.noreply.github.com>
2021-05-06 13:28:58 -0300

    Update ExercícioTrês.java

--- ExercícioTrês.java
+++ ExercícioTrês.java
@@ -1,9 +1,33 @@
 package br.com.generation.Lista.seis.cinco;
+import java.util.Scanner;
 
-public class ExercícioTrês {
+public class ExercÃ­cioTrÃªs {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        int idade, menos = 0, mais = 0, cont;
+        for(cont = 1; cont < 10; cont ++); {
+        System.out.printf("Digite sua idade: ");
+        idade = ler.nextInt();
+        }
+        while(idade != -99) {
+            if(idade<21) {
+                menos++;
+            }
+            else if(idade>50) {
+                mais++;
+            }
+            System.out.printf("Digite sua idade: ");
+            idade = ler.nextInt();
+        }
+        System.out.println("De acordo com as idade digitadas, temos: " + menos + " pessoas com menos de 21 anos e " + mais + " com mais de 50 anos.");
+        ler.close();
+    }
+    
+}
+
 
 	}
 
